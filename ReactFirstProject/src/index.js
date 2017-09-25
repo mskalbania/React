ee2a4fab3b5adde584191a379d611376de9138b5
mskalbania <cod634.steam@gmail.com>
2017-09-25 20:20:48 +0200

    Initial: project structure

---
+++ ReactFirstProject/src/index.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <h1>Test</h1>
+        );
+    }
+}
+
+React.DOM.render(<Layout/>, "main");
